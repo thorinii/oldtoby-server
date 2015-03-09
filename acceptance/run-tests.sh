35b12353cc11cb3f32632a9aabce8c0f7291962c
ThorinII <flipsidelachy@gmail.com>
2015-03-09 17:52:59 +1030

    Added script to run acceptance tests

---
+++ acceptance/run-tests.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+function check_for_server_exe {
+    if [ ! -e $1 ]; then
+        echo "$1 not found"
+        exit 1
+    else
+        echo "Found $1"
+    fi
+}
+
+
+function clean_log_directory {
+    if [ -d "log" ]; then
+        echo "Deleting old log directory"
+        rm -r log
+    fi
+
+    mkdir log
+}
+
+
+function start_server {
+    SERVER_EXE=$1
+    PORT=$2
+    SERVER_CMD="$SERVER_EXE -p $PORT"
+
+    $SERVER_CMD &> log/stdio.txt &
+    echo $!
+}
+
+
+function run_tests {
+    PORT=$1
+    sbt clean "test-only * -- -Dserver=http://localhost:$PORT/"
+}
+
+
+function kill_server {
+    PID=$1
+    kill $PID
+}
+
+
+function run_test {
+    SERVER_EXE=$1
+    PORT=$2
+
+    check_for_server_exe $SERVER_EXE
+
+    clean_log_directory
+
+    echo
+
+    echo "Starting server..."
+    SERVER_PID=$(start_server $SERVER_EXE $PORT)
+    echo "Server is PID $SERVER_PID"
+
+    echo
+
+    run_tests $PORT
+
+    echo
+
+    echo "Done test"
+
+    echo "Killing server"
+    kill_server $SERVER_PID
+}
+
+
+run_test "./oldtobyapi" 8273
